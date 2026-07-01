12b0e30f19ee32566f8586da5f4c47cd72fb65ed
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2026-07-01 00:42:57 -1000

    Add server.js and fix image path for consistency

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
