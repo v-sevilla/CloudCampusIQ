32947fdc5075e4e2f68ab30db1400ff1f3dedab1
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2026-07-07 20:02:00 -1000

    Add health endpoint for ALB health check

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
+
+app.get('/health', (req, res) => res.status(200).send('OK'))
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
